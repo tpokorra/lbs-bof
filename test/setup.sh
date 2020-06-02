25f42431c6493af4c6b3f103ed99a28beb518831
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2020-06-02 22:10:48 +0200

    another attempt to install tzdata

--- test/setup.sh
+++ test/setup.sh
@@ -3,7 +3,7 @@
 #============================
 # basic installation
 #============================
-apt-get -y install git ansible locales tzdata sudo || exit -1
+DEBIAN_FRONTEND=noninteractive apt-get -y install git ansible locales tzdata sudo || exit -1
 git clone https://github.com/ICCM-EU/BOF.git || exit -1
 cd BOF/ansible
 # perhaps update group_vars/all.yml with the actual timezone
