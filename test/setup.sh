9e8ad5db406466eb849af83aab5557c5efc8adf8
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2020-06-18 21:16:55 +0200

    fix for using the same branch not master installed from ansible

--- test/setup.sh
+++ test/setup.sh
@@ -10,6 +10,8 @@ fi
 #============================
 DEBIAN_FRONTEND=noninteractive apt-get -y install git ansible locales tzdata sudo || exit -1
 git clone https://github.com/ICCM-EU/BOF.git -b $branch || exit -1
+# we want to use the same branch for the real installation, not download the master branch in ansible
+cp -R BOF /var/www/bof
 cd BOF/ansible
 # perhaps update group_vars/all.yml with the actual timezone
 ansible-playbook playbook.yml -i localhost || exit -1
