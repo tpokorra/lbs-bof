bff322db7b602295113e1d79127d8fe6259d1251
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-06-09 17:25:38 +0200

    need libgbm-dev for cypress 5.6

--- test/setup.sh
+++ test/setup.sh
@@ -26,7 +26,7 @@ ln -s /var/www/bof
 #================================
 cd /var/www/bof
 npm install cypress || exit -1
-apt-get -y install xvfb gconf2 libgtk2.0-0 libgtk3.0 libxtst6 libxss1 libnss3 libasound2 || exit -1
+apt-get -y install xvfb gconf2 libgtk2.0-0 libgtk3.0 libxtst6 libxss1 libnss3 libasound2 libgbm-dev || exit -1
 LANG=en CYPRESS_baseUrl=http://localhost ./node_modules/.bin/cypress run --config video=false --spec 'cypress/integration/nomination.js' || exit -1
 sed -i "s/i < 60/i < 20/g" cypress/integration/voting.js
 sed -i "s/topic < 14/topic < 7/g" cypress/integration/voting.js
