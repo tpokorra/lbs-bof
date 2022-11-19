f631e75bb9cbf493385cb2070277737a0d75b80d
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-11-19 08:15:30 +0100

    for Debian Buster, we need PHP 7.4

--- test/setup.sh
+++ test/setup.sh
@@ -9,6 +9,17 @@ fi
 # basic installation
 #============================
 DEBIAN_FRONTEND=noninteractive apt-get -y install git ansible locales tzdata sudo || exit -1
+
+# on ubuntu bionic, we only have php7.2, but we need php7.3 for PHPUnit and dependancies.
+# on debian buster, we only have php7.3, but we need php7.4 for composer dependancies
+if [[ "`php --version | head -n 1 | grep -E "PHP 7.2|PHP 7.3"`" != "" ]]
+then
+  apt-get install -y software-properties-common
+  add-apt-repository ppa:ondrej/php
+  apt-get update
+  apt-get install -y php7.4 php7.4-dom php7.4-mbstring php7.4-xdebug php7.4-pdo-sqlite
+fi
+
 git clone https://github.com/ICCM-EU/BOF.git -b $branch || exit -1
 # we want to use the same branch for the real installation, not download the master branch in ansible
 mkdir -p /var/www
@@ -49,14 +60,6 @@ then
 fi
 
 ansible-playbook playbook.yml -i localhost -e 'ansible_python_interpreter=/usr/bin/python3' --extra-vars "dev=1" || exit -1
-# on ubuntu bionic, we only have php7.2, but we need php7.3 for PHPUnit and dependancies.
-if [[ "`php --version | head -n 1 | grep "PHP 7.2"`" != "" ]]
-then
-  apt-get install -y software-properties-common
-  add-apt-repository ppa:ondrej/php
-  apt-get update
-  apt-get install -y php7.4 php7.4-dom php7.4-mbstring php7.4-xdebug php7.4-pdo-sqlite
-fi
 
 cd /var/www/bof/src
 composer install --dev
