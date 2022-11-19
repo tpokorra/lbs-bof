a6de3206a63b15fe92b7590ef1fe666cc84d0cf3
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-11-19 13:46:20 +0100

    get newer composer for Debian Buster

--- test/setup.sh
+++ test/setup.sh
@@ -25,6 +25,10 @@ then
   if [[ "$OS" == "Debian GNU/Linux" ]]; then
 	wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 	echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+
+	wget -O composer-setup.php https://getcomposer.org/installer
+	php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+	export PATH=/user/local/bin:$PATH
   fi
   apt-get update
 fi
